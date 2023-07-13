e6390756e145307981adb1b364b36ccde217700f
sedoll <do11anm@naver.com>
2023-07-13 20:18:20 +0900

    230713 자바 복습

---
+++ java/ch05/src/test/Example.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Example {
+    public static void main(String[] args) {
+        A a = new A();
+        a.setM(5);
+        System.out.println(a.getM());
+        a.method();
+    }
+}
