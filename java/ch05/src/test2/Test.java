49566a80a9e3aaa9f53f80ed089c7a01f8fea880
sedoll <do11anm@naver.com>
2023-07-17 22:02:01 +0900

    230717

---
+++ java/ch05/src/test2/Test.java
@@ -0,0 +1,9 @@
+package test2;
+
+public class Test {
+    public static void main(String[] args) {
+        String data = "jeonhye0";
+        int num = Integer.parseInt(data);
+        System.out.println(num);
+    }
+}
