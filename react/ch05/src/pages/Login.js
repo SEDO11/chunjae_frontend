78101c5a355b000d9947f98c11e30c4178302864
sedoll <do11anm@naver.com>
2023-09-24 21:59:50 +0900

    230924 todo, ch05 완

--- react/ch05/src/pages/Login.js
+++ react/ch05/src/pages/Login.js
@@ -1,9 +1,41 @@
-import React from 'react'
+//Navigate : Route로 설정된 특정 페이지로 이동
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+import '../css/Login.sass'
 
-const Login = () => {
+const Login = ({ authenticated, login }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = () => {
+    try {
+      login({ email, password });
+    } catch (e) {
+      alert("Failed to login");
+      setEmail("");
+      setPassword("");
+    }
+  };
+  if (authenticated) return <Navigate to="/login" replace={true} />;
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <h1>Login</h1>
+      <input
+        value={email}
+        onChange={({ target: { value } }) => setEmail(value)}
+        type="text"
+        placeholder="email"
+      />
+      <input
+        value={password}
+        onChange={({ target: { value } }) => setPassword(value)}
+        type="password"
+        placeholder="password"
+      />
+      <button 
+        onClick={handleClick}>Login</button>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- react/ch05/src/pages/MyPage.js
+++ react/ch05/src/pages/MyPage.js
@@ -1,9 +1,19 @@
-import React from 'react'
+//Navigate == Link+replace
+//Navigate : Route로 설정된 특정 페이지로 이동
+import { Navigate } from 'react-router-dom';
 
-const MyPage = () => {
+const MyPage = ( isLoggedIn ) => {
+  //const isLoggedIn = false;
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace={true} />;
+  }
+
   return (
-    <div>MyPage</div>
-  )
-}
+    <div>
+      <h2>마이 페이지</h2>
+    </div>
+  );
+};
 
-export default MyPage
+export default MyPage;
